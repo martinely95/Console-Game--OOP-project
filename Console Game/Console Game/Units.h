b696a447c179eaddf1a15e27c67848ad426c0dac
Martin <martinely95@yahoo.com>
2015-06-10 17:37:38 +0300

    Units.h
    Added units without move and attack implementation

---
+++ Console Game/Console Game/Units.h
@@ -0,0 +1,115 @@
+#ifndef UNITS_H
+#define UNITS_H
+
+class Creature{
+public:
+	Creature() : damage(1), defense(1), health(5), mana(3), stamina(3), critChance(1), attackRange(1){};
+	Creature(int damage, int defense, int health, int mana, int stamina, int critChance, int attackRange) : 
+		damage(damage), defense(defense), health(health), mana(mana), stamina(stamina), critChance(critChance), attackRange(attackRange){};
+	
+	int GetDamage() const{
+		return damage;
+	}
+	int GetDefense() const{
+		return defense;
+	}
+	int GetHealth() const{
+		return health;
+	}
+	int GetMana() const{
+		return mana;
+	}
+	int GetStamina() const{
+		return stamina;
+	}
+	int GetCritChance() const{
+		return critChance;
+	}
+	int GetAttackRange() const{
+		return attackRange;
+	}
+	void SetDamage(int a){
+		damage = a;
+	}
+	void SetDefense(int a){
+		defense = a;
+	}
+	void SetHealth(int a){
+		health = a;
+	}
+	void SetMana(int a){
+		mana = a;
+	}
+	void SetStamina(int a){
+		stamina = a;
+	}
+	void SetCritChance(int a){
+		critChance = a;
+	}
+	void SetAttackRange(int a){
+		attackRange = a;
+	}
+	virtual void Attack() = 0;
+	virtual void Move() = 0;
+
+protected:
+	int damage;
+	int defense;  // defense определя с колко ще бъде намалена противникова атака
+	int	health;
+	int mana;
+	int stamina;  // stamina определя колко квадратчета напред може да се придвижи дадено същество
+	int critChance;  // critChance определя възможността съществото да нанесе двоен damage при атака
+	int attackRange;  // определя колко е обхвата на атаката
+};
+
+class Peasant : public Creature {
+public:
+	virtual void Attack(){
+		// още не съм го имплементирал
+	}
+	virtual void Move(){
+		//още не съм го имплементирал
+	}
+};
+
+class Footman : public Creature {
+public:
+	virtual void Attack(){
+		// още не съм го имплементирал
+	}
+	virtual void Move(){
+		// още не съм го имплементирал
+	}
+};
+
+class Archer : public Creature {
+public:
+	virtual void Attack(){
+		// още не съм го имплементирал
+	}
+	virtual void Move(){
+		// още не съм го имплементирал
+	}
+};
+
+class Griffon : public Creature {
+public:
+	virtual void Attack(){
+		// още не съм го имплементирал
+	}
+	virtual void Move(){
+		// още не съм го имплементирал
+	}
+};
+
+class Hero : public Creature {
+public:
+	virtual void Attack(){
+		// още не съм го имплементирал
+	}
+	virtual void Move(){
+		// още не съм го имплементирал
+	}
+};
+
+#endif
